35cda1789179a50b43782b71adbd3d579f9e540f
songsong <2951709680@qq.com>
2024-08-04 00:46:39 +0800

    feat:模板初始化

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import "./index.less";
-import { NavLink, Outlet } from "@/.umi/exports";
+import React from 'react';
+import './index.less';
+import { Outlet } from '@/.umi/exports';
 
 const HeaderLayout: React.FC = (props) => {
   return (
     <div>
-        <Outlet />
+      <span>layout</span>
+      <Outlet/>
     </div>
   );
 };
