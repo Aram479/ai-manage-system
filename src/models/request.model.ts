e0155f2af4b9889576fd57e4247d39b296b22e15
wangzp <wangzepeng@sharewinfo.com>
2025-03-25 11:17:58 +0800

    fix: 初始化项目

--- src/models/request.model.ts
+++
@@ -1,5 +0,0 @@
-export interface ResponseModel<T = void> {
-  data: T;
-  msg: string;
-  returnCode: string;
-}
--- src/types/model/common.model.ts
+++
@@ -1,7 +0,0 @@
-export type SafeAny = any;
-
-export interface FResponse {
-  fileName: string;
-  fileData: Blob;
-}
-
