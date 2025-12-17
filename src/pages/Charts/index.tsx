afbc73c3213851cdbcd4c5d0afdc2c10a0442f5b
Aram <1627474159@qq.com>
2025-12-17 16:19:05 +0800

    refactor(Charts): 移除页面上的静态提示文本

--- src/pages/Charts/index.tsx
+++ src/pages/Charts/index.tsx
@@ -9,9 +9,6 @@ import { Card, Flex } from "antd";
 const ChartsPage = () => {
   return (
     <div className={`${styles.chartsPage} dap-main-content`}>
-      <div style={{ fontSize: "18px", fontWeight: "bold", marginBottom: 8 }}>
-        请输入：随机生成 饼图/柱状图/折线图/瀑布图/股票图
-      </div>
       <Flex vertical gap={8}>
         <Card>
           <PieCharts />
